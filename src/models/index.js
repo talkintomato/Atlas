e6103be15652fa310a317d86e2c7699ca67bfc7c
talkintomato <darinloh@gmail.com>
2024-02-09 18:57:44 +0800

    fix pathing

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,8 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = "development";
-const config = require('./config.js'); const db = {};
+const config = require('./../config/config.js'); 
+const db = {};
 const dbConfig = config[env];
 
 const sequelize = new Sequelize(
